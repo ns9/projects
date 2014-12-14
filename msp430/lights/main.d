d9a871baf9609840192c7beb87ad26fbaa975237
navid <navid@navid-VPCSA290X.(none)>
2014-12-13 20:05:03 -0500

    Now I am become Death, the destroyer of worlds

---
+++ msp430/lights/main.d
@@ -0,0 +1,12 @@
+main.o: main.c \
+ /usr/lib/gcc/msp430/4.5.3/../../../../msp430/include/stdio.h \
+ /usr/lib/gcc/msp430/4.5.3/include/stddef.h \
+ /usr/lib/gcc/msp430/4.5.3/../../../../msp430/include/sys/types.h \
+ /usr/lib/gcc/msp430/4.5.3/../../../../msp430/include/sys/cdefs.h \
+ /usr/lib/gcc/msp430/4.5.3/../../../../msp430/include/sys/inttypes.h \
+ /usr/lib/gcc/msp430/4.5.3/../../../../msp430/include/inttypes.h \
+ /usr/lib/gcc/msp430/4.5.3/../../../../msp430/include/stdint.h \
+ /usr/lib/gcc/msp430/4.5.3/include/stdarg.h \
+ /usr/lib/gcc/msp430/4.5.3/../../../../msp430/include/msp430g2553.h \
+ /usr/lib/gcc/msp430/4.5.3/../../../../msp430/include/iomacros.h \
+ /usr/lib/gcc/msp430/4.5.3/../../../../msp430/include/in430.h
