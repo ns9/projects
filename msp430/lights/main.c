bf06f58d4142d5ed5a1568626bdb0126d24a6d8b
navid <navid@navid-VPCSA290X.(none)>
2014-12-13 20:46:32 -0500

    makefile now works, able to toggle LED

--- msp430/lights/main.c
+++ msp430/lights/main.c
@@ -5,21 +5,21 @@
 ** Author: Navid Shahrestani
 ** -------------------------------------------------------------------------*/
 
-#include  <stdio.h>
-
 #include  <msp430g2553.h>
 
 
 int main (void){
     
         WDTCTL = WDTPW + WDTHOLD;               // Stop watchdog timer                              
-        P1DIR |= 0x01;                          // Set Direction Register to OUT
+        P1DIR |= 0x01;                          // Set LED0 to output
+        P1DIR &= 0x08;                          // Set pusbutton as input
+        
         int i = 0;  
   
-        for (;;)                                  
+        while(1)                               
         {
                 P1OUT ^= 0x01;                  // Toggle P1.0
-                for(i=0; i< 20000; i++);        // Delay Loop
+                for(i=0; i< 500; i++);        // Delay Loop
         }
 
         return 0;
